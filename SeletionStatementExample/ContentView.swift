3f2219dc130f25e4a45d6f368e8882bb8379d41d
Franka Keil <fkeil@lcs.on.ca>
2021-12-02 09:09:01 -0500

    Initial Commit

---
+++ SeletionStatementExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SeletionStatementExample
+//
+//  Created by Franka Keil on 2021-12-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SeletionStatementExample/SeletionStatementExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeletionStatementExampleApp.swift
+//  SeletionStatementExample
+//
+//  Created by Franka Keil on 2021-12-02.
+//
+
+import SwiftUI
+
+@main
+struct SeletionStatementExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
